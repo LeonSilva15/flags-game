c728f02347928ca61379938cd64dd469abc5daed
LeonSilva15 <l30silva.o@gmail.com>
2025-06-29 16:38:33 -0600

    chore: setup prettier and eslint to improve code quality

--- src/components/FlagDisplay.tsx
+++ src/components/FlagDisplay.tsx
@@ -1,11 +1,11 @@
-import ReactCountryFlag from "react-country-flag";
+import ReactCountryFlag from 'react-country-flag';
 
 type Props = { code: string };
 
-export function FlagDisplay({code}: Props) {
+export function FlagDisplay({ code }: Props) {
     return (
         <div className="border shadow-md p-4">
-            <ReactCountryFlag countryCode={code} svg style={{height: '40vh', width: '30wh'}}/>
+            <ReactCountryFlag countryCode={code} svg style={{ height: '40vh', width: '30wh' }} />
         </div>
     );
 }
