92c07b76111862c859e4f2a0ed09b284f2ae9d05
LeonSilva15 <l30silva.o@gmail.com>
2025-07-05 13:51:30 -0600

    chore: store assets locally

--- src/components/FlagDisplay.tsx
+++ src/components/FlagDisplay.tsx
@@ -1,11 +1,13 @@
-import ReactCountryFlag from 'react-country-flag';
-
 type Props = { code: string };
 
 export function FlagDisplay({ code }: Props) {
     return (
-        <div className="border shadow-md p-4">
-            <ReactCountryFlag countryCode={code} svg style={{ height: '40vh', width: '30wh' }} />
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <img
+                src={`${import.meta.env.BASE_URL}flags/${code}.svg`}
+                alt={code}
+                style={{ height: '30rem' }}
+            />
         </div>
     );
 }
