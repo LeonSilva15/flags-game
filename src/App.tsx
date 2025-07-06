37912c3a006ff51948115325c75cc6adc59550d6
LeonSilva15 <l30silva.o@gmail.com>
2025-07-06 10:27:59 -0600

    feat: improve visuals; install mantine components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-import { Game } from './pages/Game';
-import './App.css';
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+
+import { Game } from './pages';
 
 export function App() {
-    return <Game />;
+    return (
+        <MantineProvider defaultColorScheme="dark">
+            <Game />
+        </MantineProvider>
+    );
 }
--- src/components/FlagDisplay.tsx
+++ src/components/FlagDisplay.tsx
@@ -1,13 +1,8 @@
+import './FlagDisplay.css';
+
 type Props = { code: string };
 
 export function FlagDisplay({ code }: Props) {
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <img
-                src={`${import.meta.env.BASE_URL}flags/${code}.svg`}
-                alt={code}
-                style={{ height: '30rem' }}
-            />
-        </div>
-    );
+    const imagePath = `${import.meta.env.BASE_URL}flags/${code}.svg`;
+    return <img className="flag" src={imagePath} alt={code} />;
 }
